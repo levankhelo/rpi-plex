e82fa51c7d66de27841a2926d4169569f9075b6f
levankhelo <levankhelo@gmail.com>
2022-05-14 23:49:12 +0400

    added directory creation commands

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash 
 
+if [[ ! -d ~/plex ]]; then
+    mkdir -p ~/plex
+    mkdir -p ~/plex/library
+    mkdir -p ~/plex/tvshow
+    mkdir -p ~/plex/movies
+fi
+
 docker compose up -d --build --env-file .env
